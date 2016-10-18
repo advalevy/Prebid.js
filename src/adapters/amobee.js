e4009859103be3c3b4a73ba0b3edc0869d8373f5
Amobee <adva@amobee.com>
2016-10-18 14:18:46 +0300

    new Amobee adapter

---
+++ src/adapters/amobee.js
@@ -0,0 +1,129 @@
+var CONSTANTS = require('../constants.json');
+var utils = require('../utils.js');
+var bidfactory = require('../bidfactory.js');
+var bidmanager = require('../bidmanager.js');
+var adloader = require('../adloader');
+
+/**
+ * Adapter for requesting bids from Amobee
+ */
+var AmobeeAdapter = function AmobeeAdapter() {
+  
+  let iframe;
+
+  // Define the bidder code
+  var amobeeBidderCode = 'amobee';
+
+  // Manage the requested and received ad units' codes, to know which are invalid (didn't return)
+  //var reqAdUnitsCode = [],
+  //    resAdUnitsCode = [];
+      
+  function _callBids(params) {
+
+    var bidRequests = params.bids || [];
+
+    // Get page data
+    //var siteDomain = window.location.host;
+    //var sitePage = window.location.href;
+
+    // Go through the requests and build array of impressions
+    utils._each(bidRequests, function(bid) {
+    	
+    	//reqAdUnitsCode.push(bid.placementCode);
+		
+        //create the iframe
+        iframe = utils.createInvisibleIframe();
+        var elToAppend = document.getElementsByTagName('head')[0];
+        
+        //insert the iframe into document
+        elToAppend.insertBefore(iframe, elToAppend.firstChild);
+
+        var iframeDoc = utils.getIframeDocument(iframe);
+        iframeDoc.write(_createRequestContent(bid));
+        iframeDoc.close();
+   
+    });
+  }
+
+  function _createRequestContent(bid) {
+  
+ 		//var domain = bid.params.amobeeDomain || 'http://rrmprod.amobee.com';
+ 		var domain = bid.params.amobeeDomain || 'http://qa-ads.integration.amobee.com'; ////for tests!!!
+ 		var adspace = bid.params.as;
+ 		var params = bid.params;
+ 		params['adw'] = (bid.sizes[0][0] || 0);
+ 		params['adh'] = (bid.sizes[0][1] || 0);
+ 		params['acc'] = params.acc || '1000004';
+ 		params['hb_bid_id'] = bid.bidId;
+ 		params['hb_placement_code'] = bid.placementCode;
+ 	
+	    var content = '<!DOCTYPE html><html><head><base target="_top" /><scr' +
+            'ipt>inDapIF=true;</scr' + 'ipt></head>';
+     
+        content += '<body>';
+        
+        //content += '<scr' + 'ipt type="text/javascript" src="http://localhost:9999/amobeejstag/tag-full.js"></scr' + 'ipt>';
+    	content += '<scr' + 'ipt type="text/javascript" src=' + domain + '/tag></scr' + 'ipt>';
+        content += '<scr' + 'ipt type="text/javascript">';
+        content += '' +
+            'var amobeeAdRequest = new amobee.adrequest({' +
+            'as: \"' + adspace + '\",' + 
+            'is_hb: true,' + 
+            'parameters: ' +
+            JSON.stringify(params) + 		
+            ',' +
+            'callback: window.parent.$$PREBID_GLOBAL$$.handleAmobeeAdCallback' +  
+            '});';
+  //      content += 'amobeeAdRequest.start();';
+
+        content += '</scr' + 'ipt>';
+        
+        content += '<scr' + 'ipt>';
+        content += '</scr' + 'ipt>';
+
+        content += '</body></html>';
+
+        return content;
+    }
+
+$$PREBID_GLOBAL$$.handleAmobeeAdCallback = function (adReceived, adRequest, adResponseHeaders, headerBiddingResponse, bidId) {
+	
+	//var bidObj = utils.getBidRequest(bidId);
+	 var bidObj = $$PREBID_GLOBAL$$._bidsRequested.find(bidSet => bidSet.bidderCode === amobeeBidderCode).bids
+          .find(bid => bid.bidId === bidId);
+
+	if (bidObj) {
+	    if (adReceived && headerBiddingResponse != null && headerBiddingResponse != '') {
+			var responseJson = JSON.parse(headerBiddingResponse);
+			var bidObject = bidfactory.createBid(1);
+			bidObject.bidderCode = amobeeBidderCode;
+			bidObject.cpm = responseJson.bidPrice;
+			bidObject.ad = responseJson.adFragment;
+			bidObject.width = bidObj.sizes[0][0];
+			bidObject.height = bidObj.sizes[0][1];
+
+			// Add the bid
+			bidmanager.addBidResponse(bidObj.placementCode, bidObject);
+			
+		} else {
+			// invalid bid response
+			bidObject = bidfactory.createBid(2);
+			bidObject.bidderCode = amobeeBidderCode;
+			bidmanager.addBidResponse(bidObj.placementCode, bidObject);
+		
+		}	
+	}
+
+	console.log("bidObj.bidId = " + bidId); ///shirel for testing
+	if (bidObj) console.log("bidObj.placementCode = " + bidObj.placementCode); ///shirel for testing
+    console.log("bidObject: " + JSON.stringify(bidObject)); ///benzi for testing
+
+};
+
+  return {
+    callBids: _callBids,
+    createRequestContent: _createRequestContent
+  };
+};
+
+module.exports = AmobeeAdapter;
